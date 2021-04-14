5b7c172c94026993b849b3322af5ed728bb5321f
Legrain Samuel <samuel.legrain@bstorm.be>
2021-04-14 12:16:54 +0200

    Add corrections Exo03 et Exo04 (partie 1)

---
+++ TB.Net.Exo03/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace TB.Net.Exo03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exo1 - Division
+            ////int x = 5, y = 2;
+
+            //Console.WriteLine("Veuillez indiquer un premier nombre :");
+            //if (int.TryParse(Console.ReadLine(), out int nb1))
+            //{
+            //    Console.WriteLine("Veuillez indiquer un second nombre :");
+            //    if (int.TryParse(Console.ReadLine(), out int nb2))
+            //    {
+
+            //        Console.WriteLine($"La division entière de {nb1} par {nb2} donne {nb1 / nb2}." +
+            //            $"\nLa division de {nb1} par {nb2} donne {(double)nb1 / nb2}." +
+            //            $"\nLe reste de la division entière de {nb1} par {nb2} donne {nb1 % nb2}.");
+
+            //        /*
+            //        int result = nb1 / nb2;
+            //        int mod = nb1 % nb2;
+            //        double div = (double)nb1 / nb2;
+            //        Console.WriteLine($"La division entière de {nb1} par {nb2} donne {result}.");
+            //        Console.WriteLine($"La division de {nb1} par {nb2} donne {div}.");
+            //        Console.WriteLine($"Le reste de la division entière de {nb1} par {nb2} donne {mod}.");
+            //        */
+            //    }
+            //    else Console.WriteLine("Ceci n'est pas un nombre.");
+            //}
+            //else Console.WriteLine("Ceci n'est pas un nombre.");
+            #endregion
+
+            #region Exo2 - BBAN
+            //Récupérer les 2 parties du BBAN sans substring()
+            //long l = 123456789012;
+            //long tenfirst = l / 100;
+            //ushort twolast =(ushort) (l % 100);
+
+            //Console.WriteLine("Veuillez introduire votre BBAN (12 chiffres):");
+            //string bban = Console.ReadLine();
+            //string bban_ten = bban.Substring(0, 10);
+            //bool bban_ten_check = ulong.TryParse(bban_ten, out ulong ten_first);
+            //string bban_two = bban.Substring(10);
+            //bool bban_two_check = ushort.TryParse(bban_two, out ushort two_last);
+
+            //ushort rest = (ushort)(ten_first % 97);
+
+            //if (rest == two_last && rest != 0) Console.WriteLine("OK");
+            //else if (rest == 0 && two_last == 97) Console.WriteLine("OK");
+            //else Console.WriteLine("KO");
+            #endregion
+
+            #region Exo3 - IBAN
+
+            Console.WriteLine("Veuillez introduire votre BBAN (12 chiffres):");
+            string bban = Console.ReadLine();
+            string bban_ten = bban.Substring(0, 10);
+            bool bban_ten_check = ulong.TryParse(bban_ten, out ulong ten_first);
+            string bban_two = bban.Substring(10);
+            bool bban_two_check = ushort.TryParse(bban_two, out ushort two_last);
+
+            ushort rest = (ushort)(ten_first % 97);
+
+            if ((rest == two_last && rest != 0) || (rest == 0 && two_last == 97)) {
+                //char[] chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'Z'];
+                ulong Be0 = ulong.Parse($"{two_last}{two_last}111400");
+                ushort two_missing = (ushort)(98 - (Be0 % 97));
+                Console.WriteLine($"L'IBAN de votre compte {bban} correspond à BE{two_missing} {bban}");
+            }
+            else Console.WriteLine("KO");
+
+            #endregion
+        }
+    }
+}
---
+++ TB.Net.Exo04/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace TB.Net.Exo04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Table de multiplication
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= 20; j++)
+            //    {
+            //        Console.Write($"{j} X {i} = {i * j} ; ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //string message = default;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 20; j++)
+            //    {
+            //        message += $"{i + 1} X {j + 1} = {(i + 1) * (j + 1)} ; "; 
+            //    }
+            //    message += "\n";
+            //}
+            //Console.WriteLine(message);
+            #endregion
+
+            #region Fibonacci
+
+            //int nb1 = 0, nb2 = 1, nb_f = nb1 + nb2, i = 3;
+            //Console.WriteLine($"F0 -> {nb1}");
+            //Console.WriteLine($"F1 -> {nb2}");
+            //Console.WriteLine($"F2 -> {nb_f}");
+            //while (i < 25) 
+            //{
+            //    nb1 = nb2;
+            //    nb2 = nb_f;
+            //    nb_f = nb1 + nb2;
+            //    Console.WriteLine($"F{i} -> {nb_f}");
+            //    i++;
+            //}
+
+            //int nb1 = 0, nb2 = 1, nb_f = nb1 + nb2, i = 3;
+            //Console.WriteLine($"F0 -> {nb1}");
+            //Console.WriteLine($"F1 -> {nb2}");
+            //Console.WriteLine($"F2 -> {nb_f}");
+            //do
+            //{
+            //    nb1 = nb2;
+            //    nb2 = nb_f;
+            //    nb_f = nb1 + nb2;
+            //    Console.WriteLine($"F{i} -> {nb_f}");
+            //    i++;
+            //} while (i < 25);
+
+            #endregion
+
+            #region Factorielle
+
+            #region test entrée numérique
+            ////bool isDigit = false;
+            ////int choice = 0;
+            ////while (!isDigit)
+            ////{
+            ////    Console.WriteLine("Veuillez introduire un nombre");
+            ////    string input = Console.ReadLine();
+            ////    isDigit = int.TryParse(input, out choice);
+            ////}
+
+            ////bool check_int = default;
+            ////uint nb = 0;
+            ////while (check_int is false)
+            ////{
+            ////    Console.WriteLine("Veuillez introduire un nombre");
+            ////    check_int = uint.TryParse(Console.ReadLine(), out nb);
+            ////}
+
+            ////uint nb;
+            ////Console.WriteLine("Veuillez introduire un nombre");
+            ////while (!uint.TryParse(Console.ReadLine(), out nb))
+            ////{
+            ////    Console.WriteLine("Ceci n'est pas un nombre, réessayer");
+            ////} 
+            #endregion
+
+            //uint nb, fact = 1;
+            //do Console.WriteLine("Veuillez introduire un nombre");
+            //while (!uint.TryParse(Console.ReadLine(), out nb));
+
+            //for (uint i = nb; i > 0; i--)
+            //{
+            //    fact *= i;
+            //}
+
+            //Console.WriteLine($"{nb}! = {fact}");
+
+            #endregion
+        }
+    }
+}
