dcee38bafd36efa4a7ce17983811706a88b13436
Legrain Samuel <samuel.legrain@bstorm.be>
2021-04-16 16:26:29 +0200

    Update Exo06-Temperatures

--- TB.Net.Exo06-Temperature/Program.cs
+++ TB.Net.Exo06-Temperature/Program.cs
@@ -9,10 +9,14 @@ namespace TB.Net.Exo06_Temperature
             Celsius c;
             c.Temperature = 18.5;
 
+            Fahrenheit c_converti = c.ToFahrenheit();
+            Console.WriteLine($"En Celsius {c.Temperature}°C , En Fahrenheit : {c_converti.Temperature}°F");
+            Console.WriteLine($"En Celsius {c.Temperature}°C , En Fahrenheit : {c.ToFahrenheit().Temperature}°F");
+
             Fahrenheit f;
             f.Temperature = 33.2;
 
-            Console.WriteLine($"En Celsius {c.Temperature} , En Fahrenheit : {f.Temperature}");
+            Console.WriteLine($"En Celsius {f.ToCelsius().Temperature}°C , En Fahrenheit : {f.Temperature}°F");
         }
     }
 }
--- TB.Net.Exo06-Temperature/Structs.cs
+++ TB.Net.Exo06-Temperature/Structs.cs
@@ -7,10 +7,24 @@ namespace TB.Net.Exo06_Temperature
     public struct Celsius
     {
         public double Temperature;
+
+        public Fahrenheit ToFahrenheit()
+        {
+            Fahrenheit f;
+            f.Temperature = (Temperature * 1.8) + 32;
+            return f;
+        }
     }
 
     public struct Fahrenheit
     {
         public double Temperature;
+
+        public Celsius ToCelsius()
+        {
+            Celsius c;
+            c.Temperature = (Temperature - 32) * (5D / 9);
+            return c;
+        }
     }
 }
